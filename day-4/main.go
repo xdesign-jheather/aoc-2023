f1f22dafa97c78e08b51f60594048b4e84a83036
Jonathan Heather <jonathan.heather@xdesign.com>
2023-12-21 12:47:37 +0000

    Day 4 Puzzle 2

--- day-4/main.go
+++ day-4/main.go
@@ -9,14 +9,23 @@ import (
 )
 
 func main() {
-	path := os.Args[1]
+	puzzle := os.Args[1]
 
-	puzzle1(path)
+	path := os.Args[2]
+
+	if puzzle == "1" {
+		puzzle1(path)
+	}
+
+	if puzzle == "2" {
+		puzzle2(path)
+	}
 }
 
 type Games []Game
 
 type Game struct {
+	ID      string
 	Winning []string
 	Have    []string
 }
@@ -72,6 +81,48 @@ func puzzle1(path string) {
 	fmt.Println(total)
 }
 
+func counter() (func(string), func() int) {
+	count := map[string]int{}
+
+	return func(s string) {
+			count[s] += 1
+		}, func() int {
+			total := 0
+			for _, n := range count {
+				total += n
+			}
+			return total
+		}
+}
+
+func puzzle2(path string) {
+	f, _ := os.Open(path)
+
+	inc, result := counter()
+
+	games := parse(f)
+
+	type recurse func(i int, f recurse)
+
+	walk := recurse(func(i int, f recurse) {
+		inc(games[i].ID)
+
+		wins := len(games[i].Won())
+
+		for off := 1; off <= wins; off++ {
+			f(i+off, f)
+		}
+	})
+
+	for i := range games {
+		walk(i, walk)
+	}
+
+	fmt.Println(result())
+
+	_ = f.Close()
+}
+
 func parse(f io.Reader) Games {
 	re1 := regexp.MustCompile("(: +| \\| +)")
 	re2 := regexp.MustCompile(" +")
@@ -86,6 +137,7 @@ func parse(f io.Reader) Games {
 		parts := re1.Split(line, -1)
 
 		games = append(games, Game{
+			ID:      parts[0],
 			Winning: re2.Split(parts[1], -1),
 			Have:    re2.Split(parts[2], -1),
 		})
